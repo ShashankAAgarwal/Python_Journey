a31ecf2f455da12bbf78208566dda331b30b950e
ShashankAAgarwal <shashankagarwal299@gmail.com>
2024-09-12 20:53:35 +0530

    update

--- 01_control_statements_assignments/q3.py
+++ 01_control_statements_assignments/q3.py
@@ -4,7 +4,7 @@ b=int(input("enter second number :"))
 if a>b :
     print("first number is greater then second")
 elif a==b :     
-    print("both are equal")
+    pass
 elif a<b :     
     print("second number is greater then first")
 
