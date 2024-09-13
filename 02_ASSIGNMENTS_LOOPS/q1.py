3b1ed1963f679bbcb68f7a482d29dbe27ab1c34d
ShashankAAgarwal <shashankagarwal299@gmail.com>
2024-09-13 13:15:25 +0530

    Add files via upload

---
+++ 02_ASSIGNMENTS_LOOPS/q1.py
@@ -0,0 +1,6 @@
+n=10 #for example let's take n= 10
+i=2
+while (i<=2*n):
+    if i%2 ==0 : 
+        print(i)
+    i+=1
---
+++ 02_ASSIGNMENTS_LOOPS/q2.py
@@ -0,0 +1,7 @@
+N= int(input("Enter the value of N : "))
+N=N+1
+for i in range(1,N):
+    if i%2 ==0 :
+        print(i)
+    i+=1
+
